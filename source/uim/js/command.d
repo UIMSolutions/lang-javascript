67278fd514b548c33f65521845c395ac4fcb2f64
uim <ui-manufaktur@gmail.com>
2019-05-27 17:49:52 +0200

    First

---
+++ source/uim/js/command.d
@@ -0,0 +1,28 @@
+﻿module uim.js.command;
+
+import uim.js;
+
+class DJSCommand : DJSRoot {
+	mixin(StringProperty!"command");
+		  
+	this() {}
+	this(string aCommand) { this.command = aCommand; }
+
+	@safe bool opEquals(string value) { return toString() == value; }
+	@safe override string toString() {
+		return _command;
+	}
+}
+auto JSCommand() { return new DJSCommand(); }
+auto JSCommand(string aCommand) { return new DJSCommand(aCommand); }
+
+string jsCommand() { return ""; }
+string jsCommand(string aCommand) { return aCommand; }
+
+unittest {
+	writeln("Testing ", __MODULE__);
+
+	assert(JSCommand() == jsCommand());
+	assert(JSCommand("var x = 'xxx';") == jsCommand("var x = 'xxx';"));
+}
+

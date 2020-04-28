13ba2d191269ad5bedad9e50f81a044ab24b7346
uim <ui-manufaktur@gmail.com>
2020-04-28 16:35:18 +0200

    Update to vibe 0.9

--- source/uim/javascript/base/package.d
+++ source/uim/javascript/base/package.d
@@ -2,21 +2,3 @@ module uim.javascript.base;
 
 import uim.javascript;
 
-@safe string toJS(T)(T[] values, bool sorted = false) {
-  string[] result;
-	if (sorted) 
-    foreach(v; values.sort) result ~= to!string(v);
-  else 
-    foreach(v; values) result ~= to!string(v);
-  return "["~result.join(",")~"]";
-}
-
-@safe string toJS(T)(T[string] values, bool sorted = false) {
-	string[] keys;
-	foreach(k, v; values) keys ~= k;
-	if (sorted) keys = keys.sort.array;
-	
-	string[] result; 
-	foreach(k; keys) result ~= k~":"~to!string(values[k]);
-	return "{"~result.join(",")~"}";
-}
